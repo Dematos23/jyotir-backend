f1d549d6e09fdc64a2d299d155fe36798e1bcd12
Diego Enrique Matos Castro <diego@MacBook-Pro-de-Diego.local>
2024-12-08 20:14:35 -0500

    cambio

--- src/middlewares/activeUserValidator.js
+++ src/middlewares/activeUserValidator.js
@@ -2,8 +2,11 @@ const findUser = require("../utils/findUser");
 
 async function activeUserValidator(req, res, next) {
   const targetUser = await findUser(req.body);
+
   if (targetUser.state !== "ACTIVO") {
-    return res.status(400).json({ message: "El usuario se encuentra inactivo" });
+    return res
+      .status(400)
+      .json({ message: "El usuario se encuentra inactivo" });
   }
 
   if (targetUser.state === "ACTIVO") {
